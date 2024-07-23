e8bdf7bace891d0c3303e95a419f7577eb0d9975
altuntaseref <seref.altuntas.ea@gmail.com>
2024-07-23 20:22:27 +0300

    day-16

---
+++ day-16/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+breaks = Turtle()
+breaks.shape("turtle")
+breaks.color("blue")
+breaks.forward(100)
+my_screen= Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+kjgkj
