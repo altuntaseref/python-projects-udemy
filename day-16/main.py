23380ae134a8cea74e98ea7070029e07875c502e
altuntaseref <seref.altuntas.ea@gmail.com>
2024-07-24 10:32:33 +0300

    day-16

--- day-16/main.py
+++ day-16/main.py
@@ -1,10 +1,17 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# breaks = Turtle()
+# breaks.shape("turtle")
+# breaks.color("blue")
+# breaks.forward(100)
+# my_screen= Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-breaks = Turtle()
-breaks.shape("turtle")
-breaks.color("blue")
-breaks.forward(100)
-my_screen= Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
-kjgkj
+from prettytable import PrettyTable
+
+my_ascii = PrettyTable()
+my_ascii.add_column("Pokemon",["Pikachu","Scurtle"])
+my_ascii.add_column("Type",["Electric","Water"])
+my_ascii.align = "r"
+print(my_ascii)
